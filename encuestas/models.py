b7d0484cf3c71ab58cb69905b8f2f92dbb0f0010
Juan Abdala <abdalajc@gmail.com>
2015-06-06 14:38:14 -0300

    completando fk de modelos

--- encuestas/models.py
+++ encuestas/models.py
@@ -13,6 +13,7 @@ class GrupoFamiliar(models.Model):
 
 
 class Entrevista(models.Model):
+    relevamiento = models.ForeignKey('Relevamiento')
     numero_entrevista = models.PositiveIntegerField()
     grupo_familiar = models.ForeignKey('GrupoFamiliar')
     entrevistador = models.ForeignKey("auth.User")
@@ -20,54 +21,56 @@ class Entrevista(models.Model):
     fecha = models.DateTimeField(auto_now=True)
 
 class Persona(models.Model):
-    nombre = models.CharField(max_length=30)
-    apellido = models.CharField(max_length=30)
-    sexo = models.CharField(max_length=30)
-    fecha_nacimiento = models.DateField()
-    nacionalidad  = models.CharField(max_length=30)
-    dni = models.IntegerField(max_length=8)
-    vinculo = models.ChoiceField(max_length=10,choices=VINCULO_TYPE)
-
     VINCULO_TYPE = (
     		('Padre','Padre'),
     		('Hijo/a','Hijo'),
     		('Madre','Madre'),
     		('Abuelo/a','Abuelo'),
 	)
+    grupo_familiar = models.ForeignKey('GrupoFamiliar')
+    
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    sexo = models.CharField(max_length=30, choices=(('m', 'masculino'), ('f', 'femenino')))
+    fecha_nacimiento = models.DateField()
+    nacionalidad  = models.CharField(max_length=30)
+    dni = models.IntegerField(max_length=8)
+    vinculo = models.ChoiceField(max_length=10,choices=VINCULO_TYPE)
+
+
 class CapitalFisico(models.Model):
-    nroPersonasPorHogar = models.IntegerField(max_length=5)
-    nroHabitaciones = models.IntegerField(max_length=5)
-
-    propietarioTerreno = models.BooleanField()
-  	situacionVivienda = models.ChoiceField(max_length=10,choices=SIT_DOMINIAL_TYPE)
-
     SIT_DOMINIAL_TYPE = (
     		('propietarioVivienda','Propietario de la Vivienda'),
     		('comodato','Comodato'),
     		('alquiler','Alquiler'),
     		('otro','Otro'),
     	)
-    pisos = models.BooleanField(label="Pisos de baldosa, cerámicos y mosaicos")
-    paredes = models.BooleanField(label="Paredes exteriores de hormigón, ladrillo o bloque con revoque o revestimiento externo")
-    techo = models.BooleanField(label="Techo de chapa de metal, fibrocemento, cielorraso, baldosa o losa")
-    gasNatural = models.BooleanField()
-    gasEnvasado = models.BooleanField()
-    leniaCarbonOtro = models.BooleanField()
+    CALEFACCION = (('gas_natural', 'Gas Natural',), ('gas_envasado', 'Gas Envasado'))
+    entrevista = models.ForeignKey('Entrevista')
+    habitaciones = models.PositiveIntegerField('Nº de Habitaciones')
+    propietario_terreno = models.BooleanField()
+  	situacion_vivienda = models.ChoiceField(max_length=50, choices=SIT_DOMINIAL_TYPE)
 
+    pisos = models.BooleanField(help_text="La vivienda tiene pisos de baldosa, cerámicos y mosaicos")
+    paredes = models.BooleanField(help_text="La vivienda tiene paredes exteriores de hormigón, ladrillo o bloque con revoque o revestimiento externo")
+    techo = models.BooleanField(help_text="Techo de chapa de metal, fibrocemento, cielorraso, baldosa o losa")
+    calefaccion = models.CharField(max_length=50, choices=CALEFACCION)
+        
 class CapitalSocial(models.Model):
-	energiaElectrica = models.BooleanField()
-	recoleccionResiduo = models.BooleanField(label="Recolección de Residuo (mínimo 2 v/sem")
-	transportePublico = models.BooleanField(label="Transporte Público <300m")
-	calleMejoradaPavimentada = models.BooleanField(label="Calle Mejorada/Pavimentada <300m")
-	jardinInfantes = models.BooleanField(label="Jardín de Infantes (<=5 cuadras)")
-	escuelaPrimaria = models.BooleanField(label="Escuela Primaria (<= 12 cuadras)")
-	escuelaSecundaria = models.BooleanField(label="Escuela Secundaria (<= 20 cuadras)")
+	entrevista = models.ForeignKey('Entrevista')
+	energia_electrica = models.BooleanField()
+	recoleccion_residuo = models.BooleanField(label="Recolección de Residuo (mínimo 2 v/sem")
+	transporte_publico = models.BooleanField(label="Transporte Público <300m")
+	calle_pavimentada = models.BooleanField(label="Calle Mejorada/Pavimentada <300m")
+	jardin_infantes = models.BooleanField(label="Jardín de Infantes (<=5 cuadras)")
+	escuela_primaria = models.BooleanField(label="Escuela Primaria (<= 12 cuadras)")
+	escuela_secundaria = models.BooleanField(label="Escuela Secundaria (<= 20 cuadras)")
 	comisaria = escuelaPrimaria = models.BooleanField(label="Comisaria (<= 50 cuadras)")
 	bomberos = models.BooleanField(label="Bomberos (<= 50 cuadras)")
 	
 class Relevamiento(models.Model):
-	fechaInicio = models.DateField()
-	fechaFinal = models.DateField()
+	fecha_inicio = models.DateField()
+	fecha_final = models.DateField()
 	zona = models.CharField(max_length=50, null=True, blank=True)
-	nombreZona = models.CharField(max_length=50, null=True, blank=True)
+	nombre_zona = models.CharField(max_length=50, null=True, blank=True, help_text="ej: Villa el Libertador)
 
