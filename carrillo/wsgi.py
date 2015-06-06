8246c52b85947ccdec9b98ee2bd782560819bd3b
Martín Gaitán <gaitan@gmail.com>
2015-06-06 15:15:18 -0300

    wsgi para python anywhere

--- carrillo/wsgi.py
+++ carrillo/wsgi.py
@@ -7,8 +7,24 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carrillo.settings")
+# TURN ON THE VIRTUAL ENVIRONMENT FOR YOUR APPLICATION
+activate_this = '/home/carrillo/.virtualenvs/carrillo/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+import os
+import sys
+
+# ADD YOUR PROJECT TO THE PYTHONPATH FOR THE PYTHON INSTANCE
+path = '/home/carrillo/carrillo'
+
+if path not in sys.path:
+    sys.path.append(path)
+
+os.chdir(path)
+
+# TELL DJANGO WHERE YOUR SETTINGS MODULE IS LOCATED
+os.environ['DJANGO_SETTINGS_MODULE'] = 'carrillo.settings'
+
+# IMPORT THE DJANGO WSGI HANDLER TO TAKE CARE OF REQUESTS
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
