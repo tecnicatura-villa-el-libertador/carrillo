41c00644705a5cce0160f058c978fd95879c83eb
Martín Gaitán <gaitan@gmail.com>
2015-06-06 16:07:09 -0300

    grupo familiar

--- encuestas/admin.py
+++ encuestas/admin.py
@@ -2,10 +2,20 @@ from django.contrib import admin
 from encuestas.models import (GrupoFamiliar, Entrevista, Persona,
                               CapitalFisico, CapitalSocial, Relevamiento)
 
-admin.site.register(GrupoFamiliar)
-admin.site.register(Entrevista)
+
+class PersonaInline(admin.TabularInline):
+    model = Persona
+
+
+class GrupoFamiliarAdmin(admin.ModelAdmin):
+    inlines = [
+        PersonaInline
+    ]
+
+
+admin.site.register(GrupoFamiliar, GrupoFamiliarAdmin)
+admin.site.register(Entrevista, EntrevistaAdmin)
 admin.site.register(CapitalSocial)
 admin.site.register(CapitalFisico)
-admin.site.register(Persona)
 admin.site.register(Relevamiento)
 
