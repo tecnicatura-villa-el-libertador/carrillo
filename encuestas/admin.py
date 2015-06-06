db8a748208795f34d4be44a8b6ed9017823a29cc
Martín Gaitán <gaitan@gmail.com>
2015-06-06 12:39:16 -0300

    Creacion del modelo grupo familiar

--- encuestas/admin.py
+++ encuestas/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from encuestas.models import GrupoFamiliar
 # Register your models here.
+
+admin.site.register(GrupoFamiliar)
--- encuestas/models.py
+++ encuestas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class GrupoFamiliar(models.Model):
+    OPCIONES_TIPO_FAMILIA = [('nuclear', 'Nuclear'),
+                             ('binuclear', 'Binuclear')]
+
+    direccion = models.CharField(max_length=100)
+    historia_clinica = models.CharField(max_length=50, null=True, blank=True)
+    telefono = models.CharField(max_length=50, null=True, blank=True)
+    tipo_familia = models.CharField(max_length=50, choices=OPCIONES_TIPO_FAMILIA)
+
