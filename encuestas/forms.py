f832e6ceaa149fcae490bf509a043b4c7ccd425d
Ruben Rodriguez <manchits20126@gmail.com>
2015-07-04 00:23:53 -0300

    Cambiamos los nombres de las clases en formularios

--- encuestas/forms.py
+++ encuestas/forms.py
@@ -2,25 +2,25 @@ from django import forms
 from models import Personas, CapitalHumano, CapitalFisico, CapitalSocial
 
 
-class Persona(forms.ModelForm):
+class PersonaModelForm(forms.ModelForm):
     class Meta:
         model = Persona
         fields = ['grupo_familiar','nombre', 'apellido', 'sexo',
                  'fecha_nacimiento', 'nacionalidad', 'dni', 'vinculo']
 
 
-class CapitalHumano(forms.ModelForm):
+class CapitalHumanoModelForm(forms.ModelForm):
     class Meta:
         model = CapitalHumano
         fields = ['entrevista', 'persona', 'trabajo', 'embarazo', 'pap',
                   'vacunas', 'coberturaMedica']
 
 
-class CapitalFisico(forms.ModelForm):
+class CapitalFisicoModelForm(forms.ModelForm):
     class Meta:
         model = CapitalFisico
 
 
-class CapitalSocial(forms.ModelForm):
+class CapitalSocialModelForm(forms.ModelForm):
     class Meta:
         model = CapitalSocial
