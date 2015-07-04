7dbe0aa5d4207c1ab4d0d5ce1873937e1ac01174
gjuncos <gabrieljuncos94@gmail.com>
2015-07-03 22:03:03 -0300

    Agrego cambios en capital social

--- encuestas/forms.py
+++ encuestas/forms.py
@@ -1,24 +1,9 @@
 from django import forms
-from models import Personas, CapitalHumano, CapitalFisico, CapitalSocial
+from .models import CapitalSocial
 
 
-class Persona(forms.ModelForm):
-    class Meta:
-        model = Personas
-
-
-class CapitalHumano(forms.ModelForm):
-    class Meta:
-        model = CapitalHumano
-
-
-class CapitalFisico(forms.ModelForm):
-    class Meta:
-        model = CapitalFisico
-
-
-class CapitalSocial(forms.ModelForm):
+class SocialModelForm(forms.ModelForm):
     class Meta:
         model = CapitalSocial
-        campos = ['entrevista','energia_electrica','recoleccion_residuo','transporte_publico','calle_pavimentada',
+        fields = ['entrevista','energia_electrica','recoleccion_residuo','transporte_publico','calle_pavimentada',
                   'jardin_infantes','escuela_primaria','escuela_secundaria','comisaria','bomberos']
--- encuestas/views.py
+++ encuestas/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-<<<<<<< HEAD
-from .forms import CapitalSocial
+
+from .forms import SocialModelForm
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render_to_response
@@ -11,22 +11,25 @@ from django.template.response import TemplateResponse
 from django.shortcuts import resolve_url
 
 
-@login_required
+
 def encuesta(request):
 
     return render_to_response('entrevista.html', locals(),context_instance=RequestContext(request))
 
 def Social(request):
-    form = CapitalSocial()
+    form = SocialModelForm()
     if request.method == 'POST':
-        form = CapitalSocial(request.POST)
-    return render(request,'clase_social.html',{'form':form})
+        form = SocialModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+    return render(request,'formulario.html',{'form':form})
     
 
 
-=======
+
 
 
 def inicio(request):
     return render(request, 'site_base.html', {})
->>>>>>> fd662f9f61b3999bab7e0a2f3afe6b204cdf58c9
+
